e0991f090c55b99e6e4cccac250b463ff7636f96
ginty1 <chiombagintykondwani@gmail.com>
2021-03-26 21:42:39 +0200

    use, State and Effect

--- components/CustomListitem.js
+++ components/CustomListitem.js
@@ -1,24 +1,42 @@
 //import liraries
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import {StyleSheet } from 'react-native';
 import {ListItem,Avatar} from 'react-native-elements';
+import {db}from '../firebase'
+
 // create a component
 const CustomListItem = ({id,chatName,enterChat}) => {
+
+    const [chatMessages, setChatMessages] = useState([]);
+
+     useEffect(() => {
+         const unsubscribe=db
+         .collection('chats')
+         .doc(id)
+         .collection('massages')
+         .orderBy('timestamp','desc')
+         .onSnapshot((snapshot)=>
+         setChatMessages(snapshot.docs.map((doc)=>doc.data())));
+         return unsubscribe;
+     });
+
+
     return (
-        <ListItem style={styles.container}>
+        <ListItem key={id} onPress={()=>enterChat(id,chatName)} key={id} bottomDivider style={styles.container}>
             <Avatar
             rounded
             source={{
-                uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+                uri:chatMessages?.[0]?.photoURL||
+                 "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
             }}
             />
 
             <ListItem.Content>
                 <ListItem.Title style={{fontWeight:'800'}}>
-                    YouTube Chat
+                    {chatName}
                 </ListItem.Title>
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail' >
-                    This is a test subtitle
+                    {chatMessages?.[0]?.displayName}:{chatMessages?.[0]?.massage}
                 </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
